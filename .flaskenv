cf840e1dcf868b961b3c6a9c4a73c33f81790466
romaindeseine <romaindeseine@gmail.com>
2020-12-16 20:26:29 +0100

    Initial hello world

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=cosoudo.py
+FLASK_ENV=development
---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv_path = os.path.join(basedir, '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # SQLALCHEMY Settings
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
