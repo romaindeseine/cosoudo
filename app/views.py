86753816fbe88df20395f5fe9ef700082525188c
romaindeseine <romaindeseine@gmail.com>
2020-12-16 21:35:13 +0100

    ajout du home html

--- app/views.py
+++ app/views.py
@@ -4,5 +4,16 @@ main = Blueprint('main', __name__)
 
 
 @main.route('/')
-def hello_world():
-    return render_template('base.html')
+def home():
+    soutenances = [
+        'Pierre Boutaud',
+        'Romain Deseine',
+        'Cyril Falcon',
+        'Hugo Federico'
+    ]
+    return render_template('home.html', soutenances=soutenances), 200
+
+
+@main.route('/donation/<slug>', methods=['GET', 'POST'])
+def donation(slug):
+    return render_template('donation.html'), 200
