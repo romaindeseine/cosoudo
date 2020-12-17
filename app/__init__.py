f645517dec8e80b23d6db72fc3f988b02a7ff6a3
romaindeseine <romaindeseine@gmail.com>
2020-12-17 12:37:17 +0100

    ajout d'un errorhandler basique

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,5 +17,13 @@ def create_app(config_name):
 
     from .views import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('errors/500.html'), 500
 
     return app
